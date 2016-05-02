6e7095bc1766dee49f2b16da10d70f69f3c451f1
gRichardson108 <garret.richardson.481@my.csun.edu>
2016-05-02 15:12:53 -0700

    Switched parser

--- src/org/garret/columnstore/ColumnStore.java
+++ src/org/garret/columnstore/ColumnStore.java
@@ -4,6 +4,8 @@ import java.util.*;
 import java.util.zip.Deflater;
 import java.io.*;
 
+import com.opencsv.CSVReader;
+
 public class ColumnStore {
 	
 	private class DictionaryEncoder {
@@ -33,28 +35,37 @@ public class ColumnStore {
 	    }
 	}
 	
-	public static List<String[]> processCSVFile(File file) throws FileNotFoundException{
-		List<String[]> csvData = null;
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		String line = "";
+	public static String[][] processCSVFile(File file) throws IOException{
+		CSVReader reader = new CSVReader(new FileReader(file));
+		String [] nextLine;
+		nextLine = reader.readNext();
+		int columnCount = nextLine.length;
 		int lineCount = countLines(file);
-		try {
-			while ((line = br.readLine()) != null){
-				String[] columns = line.split(",");
-				System.out.println(Arrays.toString(columns));				
+		String[][] data = new String [columnCount][lineCount];
+		
+		for (int row = 0; row < lineCount; row++){
+			for (int col = 0; col < columnCount; col++){
+				data[col][row] = nextLine[col];
 			}
-			Deflater compressor = new Deflater();
-			compressor.setInput(b);
-			br.close();
-		} catch (IOException e){
-			e.printStackTrace();
-		} 
-		return csvData;
+			nextLine = reader.readNext();
+			if (nextLine == null){
+				break;
+			}
+		}
+		
+		reader.close();
+		
+		return data;
 	}
 	
-	public static void main(String[] args) throws FileNotFoundException{
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		File file = new File("C:\\Users\\Allan\\Desktop\\TestCSVprint.csv");
-		processCSVFile(file);
+		String [][] data = processCSVFile(file);
+		
+		System.out.println("--- Pivoted columns: ---");
+		for (int col = 0; col < data.length; col++){
+			System.out.println(Arrays.toString(data[col]));
+		}
 	}
 }
