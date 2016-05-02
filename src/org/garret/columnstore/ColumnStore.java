675a3fa523181b1c3f841b9c3257ed52c20b271d
gRichardson108 <garret.richardson.481@my.csun.edu>
2016-05-02 15:38:33 -0700

    dictionary compression
    naive dictionary compression

--- src/org/garret/columnstore/ColumnStore.java
+++ src/org/garret/columnstore/ColumnStore.java
@@ -8,11 +8,15 @@ import com.opencsv.CSVReader;
 
 public class ColumnStore {
 	
-	private class DictionaryEncoder {
-		Map<String, Integer> dictionary = new Hashtable<String, Integer>();
+	public static String addToDictionary(String s, ArrayList<String> dictionary){
+		int location = dictionary.indexOf(s);
+		if (location == -1){
+			dictionary.add(s);
+			location = dictionary.size() - 1;
+		}
 		
+		return Integer.toString(location);
 	}
-	
 	
 	public static int countLines(File file) throws IOException {
 	    InputStream is = new BufferedInputStream(new FileInputStream(file));
@@ -58,14 +62,38 @@ public class ColumnStore {
 		return data;
 	}
 	
+	public static void printDataArray(String [][] data){
+		for (int col = 0; col < data.length; col++){
+			System.out.println(Arrays.toString(data[col]));
+		}
+	}
+	
+	public static void printDictionaries(ArrayList<ArrayList<String>> dictionaries){
+		
+	}
+	
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		File file = new File("C:\\Users\\Allan\\Desktop\\TestCSVprint.csv");
 		String [][] data = processCSVFile(file);
 		
 		System.out.println("--- Pivoted columns: ---");
+		printDataArray(data);
+		
+		
+		ArrayList<ArrayList<String>> dictionaries = new ArrayList<ArrayList<String>>();
 		for (int col = 0; col < data.length; col++){
 			System.out.println(Arrays.toString(data[col]));
+			ArrayList<String> dictionary = new ArrayList<String>();
+			for (int row = 1; row < data[col].length; row++){
+				data[col][row] = addToDictionary(data[col][row], dictionary);
+			}
+			dictionaries.add(dictionary);
 		}
+		
+		System.out.println("--- Dictionary compressed ---");
+		printDataArray(data);
+		
+		
 	}
 }
